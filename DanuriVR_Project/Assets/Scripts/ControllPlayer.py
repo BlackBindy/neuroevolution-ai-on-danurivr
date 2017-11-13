d08d63ab2e521163496c62392b622e39c92c1479
DESKTOP-DV2DAFA\jiwon <hanajitsu94@gmail.com>
2017-11-13 20:33:03 +0900

    camera controll

---
+++ DanuriVR_Project/Assets/Scripts/ControllPlayer.py
@@ -0,0 +1,71 @@
+import math
+import Math3d
+class ControllPlayer(Actor.Actor):
+	def __init__(self):
+		self.cam = Container(0)
+		self.enemy = Container(0)
+		self.bomb_con = Container(0)
+
+		self._pos = Math3d.Vector3(0)
+		self._targetDir = Math3d.Vector3(0)
+	def OnCreate(self, uid):
+		self._radius = 38
+		self._angle = 0
+		self._pos.y = self.cam.FindComponentByType("TransformGroup").GetPosition().y
+		self._Up = self.cam.FindComponentByType("TransformGroup").GetUp()
+		self._look = 0
+		self._targetDir = (0,0,0)
+
+		#load prefab
+		self._bomb = self.bomb_con.LoadPrefab("$project/Assets/Bomb.prefab")
+		print(self._bomb)
+		return 0
+
+	def Update(self):
+		#move Camera
+		self._pos.x = self._radius * math.cos(math.radians(self._angle))
+		self._pos.z = self._radius * math.sin(math.radians(self._angle))
+		
+		self.cam.FindComponentByType("TransformGroup").SetPosition(self._pos)
+
+		#Camera look at
+		self._targetDir = self.GetLocalDir(self._pos)
+		self.cam.FindComponentByType("TransformGroup").LookAtLocalDirection(self._targetDir)
+		
+		
+		#shoot
+		self._bomb.PropInstance.SetShow(True)
+		self._bomb.FindComponentByType("TransformGroup").SetPosition(Math3d.Vector3(0,10,0))
+		#print(self._bomb.FindComponentByType("TransformGroup").IsVisible())
+
+		return 0
+
+	def OnMessage(self, msg, number, Vector4_lparm, Vector4_wparam):
+		if (msg == "KeyDown") :
+			if (number == 37): #left arrow
+				self._angle -= 1
+			elif (number == 39) : #right arrow
+				self._angle += 1
+			elif (number == 38): #up arrow
+				self._look += 0.5
+			elif (number == 40): #down arrow
+				self._look -= 0.5
+
+		self._pos.x = self._radius * math.cos(math.radians(self._angle))
+		self._pos.z = self._radius * math.sin(math.radians(self._angle))
+
+	def GetLocalDir(self, v1):
+		temp = Math3d.Vector3(0)
+		v = Math3d.Vector3(0)
+
+		#Get local direction of (0,0,0)
+		temp.x = -v1.x
+		temp.y = 0
+		temp.z = -v1.z
+
+		#Calculate rotation
+		v.y = temp.y
+		v.x = temp.x * math.cos(math.radians(self._look)) - temp.z * math.sin(math.radians(self._look))
+		v.z = temp.x * math.sin(math.radians(self._look)) + temp.z * math.cos(math.radians(self._look))
+		return v
+
